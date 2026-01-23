c539ee5cba5116ed544e40e3a4ff98203616bb5b
Tobias Andresen <tiltobias@gmail.com>
2026-01-23 12:15:42 +0100

    prepare_data uses kommune_data_model.json to create kommune_data.json

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -7,20 +7,45 @@ sheet = "KomRang17_2000"
 out_path = "./frontend/public/data/kommune_data.json"
 
 
+# Load data model
+dm = json.load(open("./scripts/kommune_data_model.json", 'r', encoding='utf-8'))
+
 df = pd.read_excel(file, sheet_name=sheet)
+
 
 kommune_data = {}
 
 for index, row in df.iterrows():
-    iKomNr = str(row['iKomNr']).zfill(4) # Ensure 4-digit kommune number #TODO: settle on format, keep consistent with frontend
-    kommune_data[iKomNr] = {
-        'Navn': row['KomNavn'],
-        'Risk2000': row['Risk2000'],
+    iKomNr = str(row["iKomNr"]).zfill(4) # Ensure 4-digit kommune number #TODO: settle on format, keep consistent with frontend
+    row_data = {
+        "Navn": row["KomNavn"],
+        "sumMetric": {
+            "name": dm["name"],
+            "value": row[dm["col_name"]],
+        },
+        "elements": [],
     }
+    for element in dm["elements"]: #TODO: maybe rename element to indeks
+        element_data = {
+            "name": element["name"],
+            "value": row[element["col_name"]], #TODO: add element weighting, as well as metric weightings
+            "metrics": [],
+        }
 
-# print(df)
-# print(df.columns)
-# print(df.iloc[0])
+        for metric in element["metrics"]: #TODO: maybe rename metric to indikator
+            metric_data = {
+                "name": metric["name"],
+                "value": row[metric["col_name"]],
+            }
+            element_data["metrics"].append(metric_data)
+        
+        row_data["elements"].append(element_data)
+    
+    kommune_data[iKomNr] = row_data
+
+print(df)
+print(df.columns)
+print(df.iloc[0])
 
 # print(kommune_data)
 
