90889aa82862744a16e67f498e86698fcf2a9c77
Tobias Andresen <tiltobias@gmail.com>
2026-07-02 12:02:13 +0200

    font coordinated with klimamonitor.no

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import "@fontsource/source-sans-pro/400.css";
+import "@fontsource/source-sans-pro/700.css";
+import "@fontsource/raleway/700.css";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
