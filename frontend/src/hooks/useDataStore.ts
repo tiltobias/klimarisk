5db98a18f87b6901d3ee92012afc80c24c6ba2f7
Tobias Andresen <tiltobias@gmail.com>
2026-07-02 12:34:45 +0200

    orange choropleth and blue UI

--- frontend/src/hooks/useDataStore.ts
+++ frontend/src/hooks/useDataStore.ts
@@ -88,11 +88,11 @@ const sumInvertibleValues = (metrics: Metric[], kommune: KommuneData): number =>
 }
 
 const defaultRiskColors = [
-  '#fff5f0',
-  '#fcbba1',
-  '#fb6a4a',
-  '#cb181d',
-  '#67000d'
+  '#f7ded0',
+  '#f5b490',
+  '#ec844c',
+  '#e35205',
+  '#a63d05'
 ]
 
 interface DataStore {
