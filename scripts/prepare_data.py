2c6bdc6bbcd330de22a4f45709a963e386578458
Tobias Andresen <tiltobias@gmail.com>
2026-07-06 16:03:57 +0200

    cleaned prepare_data.py

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,10 +1,6 @@
 import pandas as pd
 import json
 
-file = "./scripts/tblKommunerangering2026.xlsx"
-out_path = "./frontend/public/data/kommune_data.json"
-out_path_model = "./frontend/public/data/kommune_data_model.json"
-
 
 def getYearSheet(year: str):
     return f"KomRang_{year}"
@@ -13,80 +9,89 @@ def getIndicatorColumn(indicator: str, year: str):
     return f"{indicator}_{year}_0_100"
 
 
-# Workaround column names containg year, TODO: possibly define changed excel norm
-def fixKey(key: str, year: str):
-    if year == "2000": return key
-    return key.replace("2000", year)
-
-# Load data model
-dm = json.load(open("./scripts/kommune_data_model.json", 'r', encoding='utf-8'))
-
-kommune_data = {
-    "years": {}
-}
-
-for year in dm["years"]:
-    df = pd.read_excel(file, sheet_name=getYearSheet(year["key"]))
-
-    kommune_data_year = {
-        "byKommune": {},
-        "byMetric": {},
+def buildDataObject(excel_file_path: str, dm: dict) -> dict:
+    kommune_data = {
+        "years": {}
     }
-    for index, row in df.iterrows():
-        iKomNr = str(row["iKomNr"]).zfill(4) # Ensure 4-digit kommune number
 
-        kommune_data_year_byKommune = {
-            "klimarisk_name": row["KomNavn"],
-            "klimarisk_indicator_number": {},
+    for year in dm["years"]:
+        df = pd.read_excel(excel_file_path, sheet_name=getYearSheet(year["key"]))
+
+        kommune_data_year = {
+            "byKommune": {},
+            "byMetric": {},
         }
-        for determinant in dm["determinants"]:
-            for indicator in determinant["indicators"]:
-                indicator_value = row[getIndicatorColumn(indicator["key"], year["key"])]
-                if pd.isna(indicator_value):
-                    continue
+        for _, row in df.iterrows():
+            iKomNr = str(row["iKomNr"]).zfill(4) # Ensure 4-digit kommune number
 
-                kommune_data_year_byKommune["klimarisk_indicator_number"][determinant["key"]] = kommune_data_year_byKommune["klimarisk_indicator_number"].get(determinant["key"], 0) + 1
-                kommune_data_year_byKommune[indicator["key"]] = indicator_value
+            kommune_data_year_byKommune = {
+                "klimarisk_name": row["KomNavn"],
+                "klimarisk_indicator_number": {},
+            }
+            for determinant in dm["determinants"]:
+                for indicator in determinant["indicators"]:
+                    indicator_value = row[getIndicatorColumn(indicator["key"], year["key"])]
+                    if pd.isna(indicator_value):
+                        continue
 
-                # Add metric [] to byMetric dictionary if it doesnt exist
-                if indicator["key"] not in kommune_data_year["byMetric"]:
-                    kommune_data_year["byMetric"][indicator["key"]] = [indicator_value]
-                else:
-                    kommune_data_year["byMetric"][indicator["key"]].append(indicator_value)
+                    kommune_data_year_byKommune["klimarisk_indicator_number"][determinant["key"]] = kommune_data_year_byKommune["klimarisk_indicator_number"].get(determinant["key"], 0) + 1
+                    kommune_data_year_byKommune[indicator["key"]] = indicator_value
 
-        kommune_data_year["byKommune"][iKomNr] = kommune_data_year_byKommune
+                    # Add metric [] to byMetric dictionary if it doesnt exist
+                    if indicator["key"] not in kommune_data_year["byMetric"]:
+                        kommune_data_year["byMetric"][indicator["key"]] = [indicator_value]
+                    else:
+                        kommune_data_year["byMetric"][indicator["key"]].append(indicator_value)
 
-    # sort byMetric {} metrics
-    for metric in kommune_data_year["byMetric"]:
-        kommune_data_year["byMetric"][metric].sort()
+            kommune_data_year["byKommune"][iKomNr] = kommune_data_year_byKommune
 
-    kommune_data["years"][year["key"]] = kommune_data_year
+        # sort byMetric {} metrics
+        for metric in kommune_data_year["byMetric"]:
+            kommune_data_year["byMetric"][metric].sort()
 
-with open(out_path, 'w', encoding='utf-8') as f:
-    json.dump(kommune_data, f, ensure_ascii=False, indent=2)
+        kommune_data["years"][year["key"]] = kommune_data_year
+    return kommune_data
+
 
 # Recreate the data model with only useful information for the frontend
-kommune_data_model = {
-    "elements": [{
-        "key": determinant["key"],
-        "name": determinant["name"],
-        **({"description": determinant["description"]} if "description" in determinant else {}),
-        **({"invert": determinant["inverted"]} if "inverted" in determinant else {}),
-        "metrics": [{
-            "key": indicator["key"],
-            "name": indicator["name"],
-            **({"description": indicator["description"]} if "description" in indicator else {}),
-            **({"url": indicator["url"]} if "url" in indicator else {}),
-            **({"invert": indicator["invert"]} if "invert" in indicator else {}),
-        } for indicator in determinant["indicators"]],
-    } for determinant in dm["determinants"]],
+def cleanDataModel(dm):
+    return {
+        "elements": [{
+            "key": determinant["key"],
+            "name": determinant["name"],
+            **({"description": determinant["description"]} if "description" in determinant else {}),
+            **({"invert": determinant["inverted"]} if "inverted" in determinant else {}),
+            "metrics": [{
+                "key": indicator["key"],
+                "name": indicator["name"],
+                **({"description": indicator["description"]} if "description" in indicator else {}),
+                **({"url": indicator["url"]} if "url" in indicator else {}),
+                **({"invert": indicator["invert"]} if "invert" in indicator else {}),
+            } for indicator in determinant["indicators"]],
+        } for determinant in dm["determinants"]],
 
-    "years": [{
-        "key": year["key"],
-        "name": year["name"],
-        "description": year["description"],
-    } for year in dm["years"]],
-}
+        "years": [{
+            "key": year["key"],
+            "name": year["name"],
+            "description": year["description"],
+        } for year in dm["years"]],
+    }
 
-with open(out_path_model, "w", encoding="utf-8") as f:
-    json.dump(kommune_data_model, f, ensure_ascii=False, indent=2)
+
+
+if __name__ == "__main__":
+    in_path_excel = "./scripts/tblKommunerangering2026.xlsx"
+    in_path_model = "./scripts/kommune_data_model.json"
+    out_path = "./frontend/public/data/kommune_data.json"
+    out_path_model = "./frontend/public/data/kommune_data_model.json"
+
+    # Load data model
+    dm = json.load(open(in_path_model, 'r', encoding='utf-8'))
+
+    # Build data object and write to JSON
+    with open(out_path, 'w', encoding='utf-8') as f:
+        json.dump(buildDataObject(in_path_excel, dm), f, ensure_ascii=False, indent=2)
+
+    # Write cleaned data model to JSON
+    with open(out_path_model, "w", encoding="utf-8") as f:
+        json.dump(cleanDataModel(dm), f, ensure_ascii=False, indent=2)
