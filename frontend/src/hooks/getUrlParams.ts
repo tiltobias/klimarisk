196a26b410a14427495b02449f4f5e638b6b3cb2
Tobias Andresen <tiltobias@gmail.com>
2026-07-16 20:56:49 +0200

    embed flag now accepts any value except false, also no value

--- frontend/src/hooks/getUrlParams.ts
+++ frontend/src/hooks/getUrlParams.ts
@@ -3,4 +3,4 @@ const params = new URLSearchParams(window.location.search);
 
 type ViewMode = "normal" | "embedded";
 
-export const viewMode: ViewMode = params.get("embed") === "true" ? "embedded" : "normal";
+export const viewMode: ViewMode = params.has("embed") && params.get("embed") !== "false" ? "embedded" : "normal";
